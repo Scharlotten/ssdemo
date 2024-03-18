2e0770f1789b90e74e9225aebde3d34f39a785c6
Anna Semjen <asemjen@Annas-MacBook-Pro.local>
2024-03-18 13:29:30 -0700

    first commit

---
+++ singlestore/resources.py
@@ -0,0 +1,8 @@
+config = {
+    "host": "svc-6b698a51-5907-4814-9548-75fc29c94633-dml.aws-virginia-6.svc.singlestore.com",
+    "port": 3306,
+    "username": "admin",
+    "password": "#Valyria93",
+    "database": "dem1",
+   
+}
